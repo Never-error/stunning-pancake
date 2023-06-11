fc34851c591dd759ed3d83fb64e43de36de03967
Never-error <185323279@qq.com>
2023-06-11 12:33:58 +0800

    Cproject update

--- exchange.c
+++
@@ -1,8 +0,0 @@
-//
-// Created by WangDake on 2023/3/5.
-//
-long exchange(long *xp,long y){
-    long x = *xp;
-    *xp = y;
-    return x;
-}
--- t3.9.c
+++
@@ -1,9 +0,0 @@
-//
-// Created by WangDake on 2023/3/5.
-//
-
-long t3_9(long x,long y){
-    x <<= 4;
-    x >>= y;
-    return x;
-}
